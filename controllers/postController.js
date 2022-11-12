73bda9233509ced430e093f3fa0e5d031875c011
mchien2002 <minhchien77777@gmail.com>
2022-11-12 23:38:54 +0700

    api post

---
+++ controllers/postController.js
@@ -0,0 +1,23 @@
+const { Post } = require("../models/postModel");
+const postController = {
+    addPost: async (req, res) => {
+        try {
+        const newPost = new Post(req.body);
+        const savePost = await newPost.save();
+        res.status(200).json(savePost);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    getAllPost: async (req, res) => {
+        try {
+            const newPost = await Post.find();
+            res.status(200).json(newPost);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+}
+
+module.exports = postController
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
-const newClassController = require("../controllers/newClassController")
+const newClassController = require("../controllers/newClassController");
+const postController = require("../controllers/postController");
 
 // GET
 router.get("/new_class/index", newClassController.getAllNewClass);
+router.get("/post/index", postController.getAllPost);
 // POST
 router.post("/new_class/add", newClassController.addNewClass);
+router.post("/post/add", postController.addPost);
 
 module.exports = router;
