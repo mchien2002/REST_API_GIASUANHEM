1b0068d0b4e9c813eac6d69e5a737e3c2872101e
mchien2002 <minhchien77777@gmail.com>
2022-11-13 19:20:40 +0700

    format

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-var bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const { response } = require("express");
 const router = require("./routes/routes");
 const PORT = 8000
+var bodyParser = require("body-parser");
 dotenv.config();
 
 // CONNECT DATABASE 
 mongoose.connect((process.env.MONGOOSE_URL), () => {
-    console.log("CONNECTED MONGODB ......");
+    console.log("#########################  CONNECTED MONGODB  #########################");
 })
 // PARSER THÀNH JSON VÀ GIỚI HẠN 
 app.use(bodyParser.json({ limit: "50mb" }));
@@ -24,6 +24,5 @@ app.use((process.env.BASE_URL), router);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(PORT, () => {
-    console.log("#####################################################");
-    console.log("Server is running...");
+    console.log("#########################  SERVER IS RUNNING  #########################");
 });
