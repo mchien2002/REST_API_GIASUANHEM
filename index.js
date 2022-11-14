75c38d1fdef9428dcf4471af1bce25dfcfb0ebfd
mchien2002 <minhchien77777@gmail.com>
2022-11-15 01:28:17 +0700

    fix

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.use(morgan("common"));
 app.use((process.env.BASE_URL), router);
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("#########################  SERVER IS RUNNING  #########################");
 });
